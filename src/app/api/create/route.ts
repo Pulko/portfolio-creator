d8f075fbdc5977051969bbba054a510b62f214d9
Fedor Tkachenko <vzdbovich@gmail.com>
2025-04-06 11:35:31 +0200

    exclude auth to lib

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { createPortfolioRepo } from '@/utils/github';
 import { Octokit } from '@octokit/rest';
 
